e0de5dfdfc95bfec9f3c16e4b0dc974ce1c79c87
Julio Davila <juliodacer@gmail.com>
2022-09-25 17:59:57 -0500

    Cambios UI, y enlazando vista en el JAVA

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadUI();
+
+    }
+
+    private fun loadUI() {
+        //Logica para crear la UI
     }
 }
